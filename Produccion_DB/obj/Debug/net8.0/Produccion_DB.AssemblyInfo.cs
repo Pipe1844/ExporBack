77827359bf5e46f886decaf426069def5663431c
Alejandro <alejandro6chaves@gmail.com>
2024-10-27 20:48:00 -0600

    no se

--- Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
+++ Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0213910cdf4702fc60b9b26c28f4a51e7d7b7f91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e93e0d8ade61f0ba4b024e3593e2668a1a1ecc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
