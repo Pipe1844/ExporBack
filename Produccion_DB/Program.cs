2a9e1db90b9de9a664c9d8ecfe2c272d0df45b5c
JosueCR170 <josolorzano4@gmail.com>
2024-11-01 00:07:32 -0600

    arreglo de fatal error

--- Produccion_DB/Program.cs
+++ Produccion_DB/Program.cs
@@ -7,10 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 Env.Load();
 // Add services to the container.
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-});
+builder.Services.AddControllers();
+
 // Learn more about  configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
