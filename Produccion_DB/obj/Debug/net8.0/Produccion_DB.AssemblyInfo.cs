7df2cded18bd9bc6b66704bd9c19c0ba35a71260
Daniel <danielgonzalezpicad@gmail.com>
2025-02-05 23:58:29 -0600

    Certificaciones controller

--- Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
+++ Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d93d85cdafe97105799aec10e319cee17872ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff021a97cb419bcaa2f70c571d43a4b7b92bc7bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
