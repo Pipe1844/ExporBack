d40b42b89f1c3eb2781a395a92f9b73b6f892608
Alejandro <alejandro6chaves@gmail.com>
2025-02-16 11:25:28 -0600

    parametros

--- Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
+++ Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146ee53377eeed79603cf5edd2947869d8360d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d263d14752ba64629c89282c6cc4e79289e1aad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
