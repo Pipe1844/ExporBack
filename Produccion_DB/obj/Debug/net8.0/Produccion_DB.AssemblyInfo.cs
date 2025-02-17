8df4fe96fa39d212ea73ea86caf86412e2659dd5
Pipe1844 <pipevifa@gmail.com>
2025-02-16 19:57:07 -0600

    Nose

--- Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
+++ Produccion_DB/obj/Debug/net8.0/Produccion_DB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146ee53377eeed79603cf5edd2947869d8360d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7ef0afc8c9305e73220124c8c8351f3b869719")]
 [assembly: System.Reflection.AssemblyProductAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Produccion_DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
